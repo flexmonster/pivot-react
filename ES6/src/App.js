c664da0967eaa7fa8893584af71d5489baec5e18
Flexmonster Pivot Table <flexmonster@users.noreply.github.com>
2018-11-15 13:53:29 +0200

    Update App.js

--- ES6/src/App.js
+++ ES6/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FlexmonsterReact.Pivot toolbar={true} licenseKey="Z71F-XAJE21-361R5E-5Z6H68" componentFolder="" width="100%" report="https://cdn.flexmonster.com/reports/report.json"/>
+        <FlexmonsterReact.Pivot toolbar={true} componentFolder="https://cdn.flexmonster.com/" width="100%" report="https://cdn.flexmonster.com/reports/report.json"/>
       </div>
     );
   }
