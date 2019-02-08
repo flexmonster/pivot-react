f875e954bb89c5af7aa377317e83481a23775bcb
Flexmonster Pivot Table <github@flexmonster.com>
2019-02-08 12:01:37 +0200

    @types/flexmonster update

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -3,12 +3,23 @@ import * as FlexmonsterReact from 'react-flexmonster';
 
 
 class App extends React.Component {
+
+  private flexmonsterRef = React.createRef<FlexmonsterReact.Pivot>();
+
   public render() {
     return (
       <div className="App">
-          <FlexmonsterReact.Pivot toolbar={true} componentFolder="https://cdn.flexmonster.com/" width="100%" report="https://cdn.flexmonster.com/reports/report.json"/>
+          <FlexmonsterReact.Pivot ref={this.flexmonsterRef} toolbar={true} componentFolder="https://cdn.flexmonster.com/" ready={() => this.onReady()} width="100%" />
       </div>
     );
+  }
+
+  public onReady(): void {
+    var pivotObject = this.flexmonsterRef.current as FlexmonsterReact.Pivot;
+    pivotObject.flexmonster.load("https://cdn.flexmonster.com/reports/report.json");
+  }
+
+  public componentDidMount() {
   }
 }
 
