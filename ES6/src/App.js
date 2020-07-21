8ab3539d0dbbcecc54ab7062b0654fdeafe91f5b
VeraDidenko <verochka1998@gmail.com>
2020-07-21 13:55:30 +0300

    added buttons for navigation

--- ES6/src/App.js
+++ ES6/src/App.js
@@ -5,17 +5,26 @@ import PivotTable from './PivotTable';
 import PivotTableAndHighcharts from './PivotTableAndHighcharts';
 import APICallsAndEvents from './APICallsAndEvents';
 import PivotTableHooks from './PivotTableHooks';
+import './App.css';
 
 class App extends Component {
 
   render(){
     return (
+      <>
+        <div className="button-routes-container">
+          <a className="btn-route" href="/">Flexmonster React Class Component</a>
+          <a className="btn-route" href="/highcharts">Flexmonster Highcharts</a>
+          <a className="btn-route" href="/api-calls">Flexmonster API calls and events</a>
+          <a className="btn-route" href="/hooks">Flexmonster React Hook</a>
+        </div>
         <Router>
           <Route path="/" component={PivotTable} exact/>
           <Route path="/highcharts" component={PivotTableAndHighcharts}/>
           <Route path="/api-calls" component={APICallsAndEvents}/>
           <Route path="/hooks" component={PivotTableHooks}/>
         </Router>
+      </>
 
     );
   }
